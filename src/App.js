fc3d3ca2f278618b89719f7ea3434c692c6b9502
Bevin Richardson <bevinkylieart@gmail.com>
2020-01-07 20:30:37 -0500

    updating the readme with added components files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
-import React from "react";
+iimport React from "react";
+import ApodDisplay from "./components/ApodDisplay";
 import "./App.css";
+import styled from "styled-components";
 
+const ApodTitle = styled.h1`
+  font-size: 70px;
+  text-align: center;
+  margin: 2% 0 3% 0;
+`;
 function App() {
   return (
     <div className="App">
@@ -8,6 +15,9 @@ function App() {
         Read through the instructions in the README.md file to build your NASA
         app! Have fun 🚀!
       </p>
+      <ApodTitle>Nasa Astronomy Picture of the Day</ApodTitle>
+      <hr></hr>
+      <ApodDisplay />
     </div>
   );
 }
---
+++ src/components/ApodCard.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const CardDiv = styled.div`
+    display:flex;
+    justify-content: center;
+    @media (max-width: 800px){
+        flex-direction: column;
+    }
+`;
+const TextContainer = styled.div`
+    width: 25%;
+    @media (max-width: 800px) {
+        width: 100%;
+    }
+`;
+const CardTitle = styled.h2`
+    font-size: 50px;
+`;
+const CardImage = styled.img`
+    border: 2px solid black;
+`;
+const CardExplanation = styled.p`
+    font-size: 20px;
+    margin: 0 auto;
+    padding: 10%;
+`;
+const ApodCard = props => {
+    return (
+        <CardDiv className='apodCard'>
+            <CardImage className='apodImage' alt='Astronomy Image of the Day' src={props.url}/>
+            <TextContainer>
+            <CardTitle>{props.title}</CardTitle>
+            <hr></hr>
+            <CardExplanation>{props.explanation}</CardExplanation>
+            <p>{props.date}</p>
+            </TextContainer>
+        </CardDiv>
+    );
+};
+
+export default ApodCard 
